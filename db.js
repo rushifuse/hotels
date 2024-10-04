a79ddb9ce06dfbbeee7dd546c1956486b8cb2943
Rushikesh Fuse <fuserushikesh@gmail.com>
2024-10-05 02:02:56 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
-const mongoURL = 'mongodb://localhost:27017/hotels'    // tya url vr hotels nava cha databases create hoil
+ //const mongoURL = process.env.MONGODB_URL_LOCAL;    // tya url vr hotels nava cha databases create hoil
+ require('dotenv').config();
+const mongoURL=process.env.DB_URL;
 
-mongoose.connect(mongoURL)               // it is only use for establish the connection
+
+
+
+
+mongoose.connect(mongoURL)
+    .then(() => console.log('Database connected successfully'))
+  .catch((err) => console.error('Database connection error:', err));
+             // it is only use for establish the connection
 
 const db= mongoose.connection;                // db ha mongoose cha default connection object create kela ahe use hote jeva pn by using 
 
--- server.js
+++ server.js
@@ -135,11 +135,11 @@ const express = require('express');
 const app= express();
 const db= require('./db');
 const Person = require('./models/person');
-
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());   
-
+const PORT =process.env.PORT || 3000;
 
 const MenuItem= require('./models/MenuItem');
 const { reverse } = require('lodash');
@@ -165,7 +165,7 @@ app.use('/menu', menuRoutes);
 
 
 
-app.listen(3000,()=>
+app.listen(PORT,()=>
 {
     console.log("Server is started successfully ........");
 });
